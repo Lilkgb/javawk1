1730f07989245af4a4a496d59e3066157fcc346e
Ryan McLean <mcleanrs@comcast.net>
2019-03-15 12:33:40 -0700

    function to get users age. tested with jasmine

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,5 +1,6 @@
 import { UserAge } from './../src/backEnd.js';
 import {dateString} from './../src/backEnd.js';
+import {getAge} from './../src/backEnd.js';
 
 describe('UserAge', function() {
   it('should set users birthday', function() {
@@ -8,5 +9,14 @@ describe('UserAge', function() {
     let year = 1995;
     let newDate = dateString(month,day,year);
     expect(newDate).toEqual("may 31, 1995")
+  });
+
+  it('should set users age', function() {
+    let month = "may";
+    let day = 31;
+    let year = 1995;
+    let newDate = dateString(month,day,year);
+    let age = getAge(newDate);
+    expect(age).toEqual(23);
   });
 });
