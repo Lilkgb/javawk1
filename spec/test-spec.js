5c52ceec15c2cdcbfd5c9f64956a4f3a7c907092
Ryan McLean <mcleanrs@comcast.net>
2019-03-15 11:54:53 -0700

    add userage constructor. made functions to set month, day, year from user iput

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,1 +1,12 @@
+import { UserAge } from './../src/backEnd.js';
 
+describe('UserAge', function() {
+  it('should set users birthday', function () {
+    let user = new UserAge();
+    user.setMonth('May')
+    user.setDay(31);
+    user.setYear(1995);
+    console.log(user);
+    expect(user).toEqual('May', 31, 1995);
+  });
+});
---
+++ src/backEnd.js
@@ -0,0 +1,17 @@
+export class UserAge {
+  user(month, day, year) {
+    this.month = month;
+    this.day = day;
+    this.year = year;
+  }
+  setMonth(month) {
+    this.month = month;
+  }
+  setDay(day) {
+    this.day = day;
+  }
+  setYear(year) {
+    this.year = year;
+  }
+
+}
