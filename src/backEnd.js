f9ebd6207a60ef7315d55f21cb5977e569eb5698
Ryan McLean <mcleanrs@comcast.net>
2019-03-15 12:33:40 -0700

    function to get users age. tested with jasmine

--- src/backEnd.js
+++ src/backEnd.js
@@ -1,8 +1,15 @@
-export class UserAge {
-  
-}
-
 export function dateString(month,day,year) {
   let newDate = `${month} ${day}, ${year}`;
   return newDate;
+}
+
+export function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+  return age;
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import $ from 'jquery';
 import './input.scss';
 import {dateString} from './backEnd.js';
+import {getAge} from './backEnd.js';
 
 $(document).ready(function() {
   let month = 'may';
   let day = 31;
   let year = 1995;
   let newDate = dateString(month,day,year);
-  let user = new Date(newDate);
-  console.log(user);
+  let age = getAge(newDate);
+  console.log(age);
 });
